22bc412da325280df0c7f9a823c8d1870cda06af
QlowB <nicolas.winkler@gmx.ch>
2018-12-22 15:34:57 +0100

    added stuff to context

--- src/sem/Context.h
+++ src/sem/Context.h
@@ -2,17 +2,33 @@
 #define QLOW_SEM_CONTEXT_H
 
 #include "Type.h"
+#include "unordered_map"
 
 namespace qlow::sem
 {
     class Context;
+    
+    using TypeId = size_t;
 }
 
 
 class qlow::sem::Context
 {
 private:
+    std::vector<std::unique_ptr<Type>> types;
+    std::unordered_map<Type&, TypeId> typesMap;
     
+public:
+    
+    TypeId addType(Type&& type) {
+        if (typesMap.contains(type)) {
+            return typesMap[type];
+        }
+        else {
+            types.push_back(std::unique_ptr<Type>(type));
+            return types.size() - 1;
+        }
+    }
 };
 
 #endif // QLOW_SEM_CONTEXT_H
