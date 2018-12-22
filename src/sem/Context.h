61f31b4b9a283759f4aaa4c13fdef72aa4fc653e
Nicolas Winkler <nicolas.winkler@gmx.ch>
2018-12-22 13:01:35 +0100

    structuring the codebase

---
+++ src/sem/Context.h
@@ -0,0 +1,20 @@
+#ifndef QLOW_SEM_CONTEXT_H
+#define QLOW_SEM_CONTEXT_H
+
+#include "Type.h"
+
+namespace qlow::sem
+{
+    class Context;
+}
+
+
+class qlow::sem::Context
+{
+private:
+    
+};
+
+#endif // QLOW_SEM_CONTEXT_H
+
+
