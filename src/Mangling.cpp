a5435e2d0d97d5ba9a0b8935ca888a68992d046a
Nicolas Winkler <nicolas.winkler@gmx.ch>
2019-04-10 00:22:33 +0200

    mangler

---
+++ src/Mangling.cpp
@@ -0,0 +1,13 @@
+#include "Mangling.h"
+#include "sem/Semanic.h"
+
+using qlow::Mangler;
+
+
+std::string Mangler::mangle(const qlow::sem::Method& method)
+{
+    method.
+}
+
+
+
---
+++ src/Mangling.h
@@ -0,0 +1,25 @@
+#ifndef QLOW_MANGLING_H_
+#define QLOW_MANGLING_H_
+
+#include <string>
+
+namespace qlow
+{
+    class Mangler;
+    
+    namespace sem
+    {
+        struct Method;
+    }
+}
+
+
+class qlow::Mangler
+{
+public:
+    Mangler(void) = default;
+    
+    std::string mangle(const sem::Method& method);
+};
+
+#endif // QLOW_MANGLING_H_
